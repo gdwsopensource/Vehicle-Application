d92222ac5d0dff84e2bdebb73c2be2b07ff3e824
Shinenadeng <shinena_deng@163.com>
2017-07-13 18:04:47 +0800

    trank analysis

--- src/main/java/com/gdws/vehicle/repository/TrankAnalysisRespository.java
+++ src/main/java/com/gdws/vehicle/repository/TrankAnalysisRespository.java
@@ -28,6 +28,6 @@ public interface TrankAnalysisRespository extends JpaRepository<TrankAnalysis, I
 	 * @param endTime
 	 * @return
 	 */
-	@Query(value = "SELECT a.id as id,a.plate_type as plate_type,a.alert_type as alert_type,a.cross_direction as cross_direction,DATE_FORMAT(a.alert_time,'%Y-%m-%d %H:%m') as alert_time,b.latitude as lat,b.longitude as lng,b.cross_id as cross_id,b.cross_name as cross_name from car_cross_info a left JOIN cross_info b on a.cross_id=b.cross_id where  a.plate_no=?1 and DATE_FORMAT(a.alert_time,'%Y-%m-%d') BETWEEN ?2 and ?3  ORDER BY DATE_FORMAT(a.alert_time,'%Y-%m-%d %H:%m');", nativeQuery = true)
+	@Query(value = "SELECT a.id as id,a.plate_type as plate_type,a.alert_type as alert_type,a.cross_direction as cross_direction,DATE_FORMAT(a.alert_time,'%Y-%m-%d %H:%m') as alert_time,b.bd_latitude as lat,b.bd_longitude as lng,b.cross_id as cross_id,b.cross_name as cross_name from car_cross_info a left JOIN cross_info b on a.cross_id=b.cross_id where  a.plate_no=?1 and DATE_FORMAT(a.alert_time,'%Y-%m-%d') BETWEEN ?2 and ?3  ORDER BY DATE_FORMAT(a.alert_time,'%Y-%m-%d %H:%m');", nativeQuery = true)
 	List<TrankAnalysis> trankAnalysisOnPlateNo(String plateNo, String startTime, String endTime);
 }
